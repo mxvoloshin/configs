e7af6ea1919a5cf9fda92c45d7ba446b668719a9
mxvlshn <mx.voloshin@gmail.com>
2026-01-03 06:50:12 -0700

    Add files via upload

---
+++ qmk-config/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// Start here for ~90 WPM
+#define TAPPING_TERM 230
+
+#define TAPPING_TERM_PER_KEY
+
+// Use opposite-hands rule for tap-hold keys (fixes HRM misfires)
+#define CHORDAL_HOLD
+
+// Makes opposite-hand mod combos feel responsive, without HOLD_ON_OTHER_KEY_PRESS
+#define PERMISSIVE_HOLD
+
+// IMPORTANT: remove this (it causes your “mods 90% of the time” issue)
+// #define HOLD_ON_OTHER_KEY_PRESS
+
